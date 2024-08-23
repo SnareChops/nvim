e0568234b18c8092014c0b218e6df938d87b28af
SnareChops <snare_chops@yahoo.com>
2024-08-23 08:14:23 -0400

    Added Trouble and Surround plugins

--- init.lua
+++ init.lua
@@ -797,10 +797,19 @@ require('lazy').setup({
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-  { 'xiyaowong/transparent.nvim' },
+  { -- Transparent background
+    'xiyaowong/transparent.nvim',
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  { 'github/copilot.vim' },
+  { -- Surround Plugin
+    'kylechui/nvim-surround',
+    version = '*',
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {}
+    end,
+  },
   {
     'nvim-tree/nvim-tree.lua',
     opts = {
@@ -817,6 +826,43 @@ require('lazy').setup({
       vim.cmd [[hi NvimTreeNormalNC guibg=NONE ctermbg=NONE]]
       require('nvim-tree.api').tree.open()
     end,
+  },
+  { -- Diagnostics
+    'folke/trouble.nvim',
+    opts = {},
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
   },
 
   { -- Collection of various small independent plugins/modules
