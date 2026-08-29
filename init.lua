7cd24c3a74cf0d88aca02205857fae25c1de403e
SnareChops <snare_chops@yahoo.com>
2026-08-29 16:19:12 -0400

    ts lsp tweak

--- init.lua
+++ init.lua
@@ -827,7 +827,10 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        ts_native = {
+          cmd = { 'tsc', '--lsp', '--stdio' },
+          filetypes = { 'javascript', 'typescript' },
+        },
         --
 
         lua_ls = {
